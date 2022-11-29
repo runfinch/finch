5f70bbc9b6fc4db42cea2cc0ab80322e2b04379f
Niklas Metje <22395665+niklasmtj@users.noreply.github.com>
2022-11-29 20:53:41 +0100

    Refactor: Update run to call internal status func
    Signed-off-by: Niklas Metje <22395665+niklasmtj@users.noreply.github.com>

--- cmd/virtual_machine_status.go
+++ cmd/virtual_machine_status.go
@@ -37,32 +37,21 @@ func (sva *statusVMAction) runAdapter(cmd *cobra.Command, args []string) error {
 }
 
 func (sva *statusVMAction) run() error {
-	err := sva.assertVMIsRunning(sva.creator, sva.logger)
-	if err != nil {
-		return err
-	}
-
-	limaCmd := sva.creator.CreateWithoutStdio("ls", limaInstanceName)
-	logs, err := limaCmd.CombinedOutput()
-	if err != nil {
-		sva.logger.Errorf("Finch virtual machine status failed, debug logs: %s", logs)
-		return err
-	}
-	sva.logger.Infof("%s", logs)
-	return nil
-}
-
-func (sva *statusVMAction) assertVMIsRunning(creator command.LimaCmdCreator, logger flog.Logger) error {
-	status, err := lima.GetVMStatus(creator, logger, limaInstanceName)
+	status, err := lima.GetVMStatus(sva.creator, sva.logger, limaInstanceName)
 	if err != nil {
 		return err
 	}
 	switch status {
+	case lima.Running:
+		sva.logger.Infof("the instance %q is running", limaInstanceName)
+		return nil
 	case lima.Nonexistent:
 		return fmt.Errorf("the instance %q does not exist", limaInstanceName)
 	case lima.Stopped:
 		return fmt.Errorf("the instance %q is stopped. run `finch %s start` to start the instance", limaInstanceName, virtualMachineRootCmd)
+	case lima.Unknown:
+		return fmt.Errorf("the instance status of %q is unknown", limaInstanceName)
 	default:
-		return nil
+		return fmt.Errorf("the instance %q gave a not defined status", limaInstanceName)
 	}
 }
