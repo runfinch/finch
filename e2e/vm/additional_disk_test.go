13e424c1eae360fcce18ea5cc3f7b62e371a90ae
Ang Zhou <angzh@amazon.com>
2023-03-07 18:20:23 -0800

    remove focus
    Signed-off-by: Ang Zhou <angzh@amazon.com>

--- e2e/vm/additional_disk_test.go
+++ e2e/vm/additional_disk_test.go
@@ -15,54 +15,55 @@ import (
 const (
 	savedImage    = "public.ecr.aws/docker/library/alpine:latest"
 	containerName = "userDataTest"
-	volumeName    = "test-volume"
+	volumeName    = "userVolume"
+	networkName   = "userNetwork"
 	text          = "foo"
 )
 
 var testAdditionalDisk = func(o *option.Option) {
 	ginkgo.Describe("Additional disk", ginkgo.Serial, func() {
-		ginkgo.FIt("should retain all user data and able to start the created container after the VM is deleted", func() {
+		ginkgo.It("should retain all user data and able to start the created container after the VM is deleted", func() {
 			command.Run(o, "pull", savedImage)
 			gomega.Expect(command.StdoutAsLines(o, "images", "--format", "{{.Name}}")).
 				Should(gomega.ContainElement(savedImage))
+			ginkgo.DeferCleanup(command.Run, o, "rmi", savedImage)
 
 			command.Run(o, "volume", "create", volumeName)
 			gomega.Expect(command.StdoutAsLines(o, "volume", "ls", "--format", "{{.Name}}")).
 				Should(gomega.ContainElement(volumeName))
+			ginkgo.DeferCleanup(command.Run, o, "volume", "rm", volumeName)
 
-			//command.Run(o, "network", "create", networkName)
-			//oldNetworkOutput := command.StdoutAsLines(o, "network", "ls", "--format", "{{.Name}}")
-			//gomega.Expect(oldNetworkOutput).Should(gomega.ContainElement(networkName))
+			command.Run(o, "network", "create", networkName)
+			gomega.Expect(command.StdoutAsLines(o, "network", "ls", "--format", "{{.Name}}")).
+				Should(gomega.ContainElement(networkName))
+			ginkgo.DeferCleanup(command.Run, o, "network", "rm", networkName)
 
 			command.Run(o, "run", "-d", "--name", containerName, "-v", fmt.Sprintf("%s:/tmp", volumeName), savedImage,
 				"sh", "-c", fmt.Sprintf("echo %s > /tmp/test.txt; sleep infinity", text))
-			oldPsOutput := command.StdoutAsLines(o, "ps", "--all", "--format", "{{.Names}}")
-			gomega.Expect(oldPsOutput).Should(gomega.ContainElement(containerName))
+			ginkgo.DeferCleanup(command.Run, o, "rm", "-f", containerName)
+
+			gomega.Expect(command.StdoutAsLines(o, "ps", "--all", "--format", "{{.Names}}")).
+				Should(gomega.ContainElement(containerName))
 
 			command.New(o, virtualMachineRootCmd, "stop", "-f").WithoutCheckingExitCode().WithTimeoutInSeconds(90).Run()
 			command.Run(o, virtualMachineRootCmd, "remove")
 			command.New(o, virtualMachineRootCmd, "init").WithTimeoutInSeconds(240).Run()
 
-			newImagesOutput := command.StdoutAsLines(o, "images", "--format", "{{.Name}}")
-			gomega.Expect(newImagesOutput).Should(gomega.ContainElement(savedImage))
+			imagesOutput := command.StdoutAsLines(o, "images", "--format", "{{.Name}}")
+			gomega.Expect(imagesOutput).Should(gomega.ContainElement(savedImage))
 
-			newPsOutput := command.StdoutAsLines(o, "ps", "--all", "--format", "{{.Names}}")
-			gomega.Expect(newPsOutput).Should(gomega.ContainElement(containerName))
+			psOutput := command.StdoutAsLines(o, "ps", "--all", "--format", "{{.Names}}")
+			gomega.Expect(psOutput).Should(gomega.ContainElement(containerName))
 
-			newVolumeOutput := command.StdoutAsLines(o, "volume", "ls", "--format", "{{.Name}}")
-			gomega.Expect(newVolumeOutput).Should(gomega.ContainElement(volumeName))
+			volumeOutput := command.StdoutAsLines(o, "volume", "ls", "--format", "{{.Name}}")
+			gomega.Expect(volumeOutput).Should(gomega.ContainElement(volumeName))
 
-			//newNetworkOutput := command.StdoutAsLines(o, "network", "ls", "--format", "{{.Name}}")
-			//gomega.Expect(newNetworkOutput).Should(gomega.ContainElement(oldNetworkOutput))
+			networkOutput := command.StdoutAsLines(o, "network", "ls", "--format", "{{.Name}}")
+			gomega.Expect(networkOutput).Should(gomega.ContainElement(networkName))
 
 			command.Run(o, "start", containerName)
 			textOutput := command.StdoutStr(o, "exec", containerName, "cat", "/tmp/test.txt")
 			gomega.Expect(textOutput).Should(gomega.Equal(text))
-
-			//ginkgo.DeferCleanup(command.Run, o, "network", "rm", networkName)
-			ginkgo.DeferCleanup(command.Run, o, "rmi", savedImage)
-			ginkgo.DeferCleanup(command.Run, o, "volume", "rm", volumeName)
-			ginkgo.DeferCleanup(command.Run, o, "rm", "-f", containerName)
 		})
 	})
 }
