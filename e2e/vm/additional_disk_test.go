349c93dd9bd9a78e9ad17f6202ecb8ff76c1edc2
Ang Zhou <angzh@amazon.com>
2023-03-07 17:32:45 -0800

    Improve test coverage for additional disk
    Signed-off-by: Ang Zhou <angzh@amazon.com>

--- e2e/vm/additional_disk_test.go
+++ e2e/vm/additional_disk_test.go
@@ -4,6 +4,8 @@
 package vm
 
 import (
+	"fmt"
+
 	"github.com/onsi/ginkgo/v2"
 	"github.com/onsi/gomega"
 	"github.com/runfinch/common-tests/command"
@@ -13,31 +15,54 @@ import (
 const (
 	savedImage    = "public.ecr.aws/docker/library/alpine:latest"
 	containerName = "userDataTest"
+	volumeName    = "test-volume"
+	text          = "foo"
 )
 
 var testAdditionalDisk = func(o *option.Option) {
 	ginkgo.Describe("Additional disk", ginkgo.Serial, func() {
-		ginkgo.It("Retains container user data after the VM is deleted", func() {
+		ginkgo.FIt("should retain all user data and able to start the created container after the VM is deleted", func() {
 			command.Run(o, "pull", savedImage)
-			ginkgo.DeferCleanup(command.Run, o, "rmi", savedImage)
-			oldImagesOutput := command.StdoutStr(o, "images", "--format", "{{.Name}}")
-			gomega.Expect(oldImagesOutput).Should(gomega.ContainSubstring(savedImage))
+			gomega.Expect(command.StdoutAsLines(o, "images", "--format", "{{.Name}}")).
+				Should(gomega.ContainElement(savedImage))
 
-			command.Run(o, "run", "--name", containerName, savedImage)
-			ginkgo.DeferCleanup(command.Run, o, "rm", containerName)
-			oldPsOutput := command.StdoutStr(o, "ps", "--all", "--format", "{{.Names}}")
-			gomega.Expect(oldPsOutput).Should(gomega.ContainSubstring(containerName))
+			command.Run(o, "volume", "create", volumeName)
+			gomega.Expect(command.StdoutAsLines(o, "volume", "ls", "--format", "{{.Name}}")).
+				Should(gomega.ContainElement(volumeName))
 
-			command.New(o, virtualMachineRootCmd, "stop").WithoutCheckingExitCode().WithTimeoutInSeconds(90).Run()
+			//command.Run(o, "network", "create", networkName)
+			//oldNetworkOutput := command.StdoutAsLines(o, "network", "ls", "--format", "{{.Name}}")
+			//gomega.Expect(oldNetworkOutput).Should(gomega.ContainElement(networkName))
+
+			command.Run(o, "run", "-d", "--name", containerName, "-v", fmt.Sprintf("%s:/tmp", volumeName), savedImage,
+				"sh", "-c", fmt.Sprintf("echo %s > /tmp/test.txt; sleep infinity", text))
+			oldPsOutput := command.StdoutAsLines(o, "ps", "--all", "--format", "{{.Names}}")
+			gomega.Expect(oldPsOutput).Should(gomega.ContainElement(containerName))
+
+			command.New(o, virtualMachineRootCmd, "stop", "-f").WithoutCheckingExitCode().WithTimeoutInSeconds(90).Run()
 			command.Run(o, virtualMachineRootCmd, "remove")
-
 			command.New(o, virtualMachineRootCmd, "init").WithTimeoutInSeconds(240).Run()
 
-			newImagesOutput := command.StdoutStr(o, "images", "--format", "{{.Name}}")
-			gomega.Expect(newImagesOutput).Should(gomega.Equal(oldImagesOutput))
+			newImagesOutput := command.StdoutAsLines(o, "images", "--format", "{{.Name}}")
+			gomega.Expect(newImagesOutput).Should(gomega.ContainElement(savedImage))
 
-			newPsOutput := command.StdoutStr(o, "ps", "--all", "--format", "{{.Names}}")
-			gomega.Expect(newPsOutput).Should(gomega.Equal(oldPsOutput))
+			newPsOutput := command.StdoutAsLines(o, "ps", "--all", "--format", "{{.Names}}")
+			gomega.Expect(newPsOutput).Should(gomega.ContainElement(containerName))
+
+			newVolumeOutput := command.StdoutAsLines(o, "volume", "ls", "--format", "{{.Name}}")
+			gomega.Expect(newVolumeOutput).Should(gomega.ContainElement(volumeName))
+
+			//newNetworkOutput := command.StdoutAsLines(o, "network", "ls", "--format", "{{.Name}}")
+			//gomega.Expect(newNetworkOutput).Should(gomega.ContainElement(oldNetworkOutput))
+
+			command.Run(o, "start", containerName)
+			textOutput := command.StdoutStr(o, "exec", containerName, "cat", "/tmp/test.txt")
+			gomega.Expect(textOutput).Should(gomega.Equal(text))
+
+			//ginkgo.DeferCleanup(command.Run, o, "network", "rm", networkName)
+			ginkgo.DeferCleanup(command.Run, o, "rmi", savedImage)
+			ginkgo.DeferCleanup(command.Run, o, "volume", "rm", volumeName)
+			ginkgo.DeferCleanup(command.Run, o, "rm", "-f", containerName)
 		})
 	})
 }
