d5525ca5a3f15bc9e5c3e8e879e2d1dab0171c5b
Ziwen Ning <ningziwe@amazon.com>
2023-04-27 14:45:54 -0700

    fix lint
    Signed-off-by: Ziwen Ning <ningziwe@amazon.com>

--- benchmark/suite.go
+++ benchmark/suite.go
@@ -21,10 +21,12 @@ const (
 	testContainerName     = "ctr-test"
 )
 
+// Suite is a struct that groups benchmark functions and shared state.
 type Suite struct {
 	subject string
 }
 
+// Setup initializes the Suite by getting the subject.
 func (suite *Suite) Setup() error {
 	subject, err := GetSubject()
 	if err != nil {
@@ -34,22 +36,27 @@ func (suite *Suite) Setup() error {
 	return nil
 }
 
+// InitVM initializes a virtual machine.
 func (suite *Suite) InitVM() error {
 	return exec.Command(suite.subject, virtualMachineRootCmd, "init").Run() // #nosec G204
 }
 
+// StartVM starts the VM.
 func (suite *Suite) StartVM() error {
 	return exec.Command(suite.subject, virtualMachineRootCmd, "start", "-f").Run() // #nosec G204
 }
 
+// StopVM stops the VM.
 func (suite *Suite) StopVM() error {
 	return exec.Command(suite.subject, virtualMachineRootCmd, "stop", "-f").Run() // #nosec G204
 }
 
+// RemoveVM removes the VM.
 func (suite *Suite) RemoveVM() error {
 	return exec.Command(suite.subject, virtualMachineRootCmd, "remove", "-f").Run() // #nosec G204
 }
 
+// BenchmarkVMInit measures the metrics to initialize the VM.
 func (suite *Suite) BenchmarkVMInit(b *testing.B) {
 	Wrapper(b, func() {
 		assert.NoError(b, exec.Command(suite.subject, virtualMachineRootCmd, "init").Run()) //nolint:gosec // testing only
@@ -59,6 +66,7 @@ func (suite *Suite) BenchmarkVMInit(b *testing.B) {
 	})
 }
 
+// BenchmarkVMStart measures the metrics to start the VM.
 func (suite *Suite) BenchmarkVMStart(b *testing.B) {
 	assert.NoError(b, exec.Command(suite.subject, virtualMachineRootCmd, "init").Run())       //nolint:gosec // testing only
 	assert.NoError(b, exec.Command(suite.subject, virtualMachineRootCmd, "stop", "-f").Run()) //nolint:gosec // testing only
@@ -71,6 +79,7 @@ func (suite *Suite) BenchmarkVMStart(b *testing.B) {
 	assert.NoError(b, exec.Command(suite.subject, virtualMachineRootCmd, "remove", "-f").Run()) //nolint:gosec // testing only
 }
 
+// BenchmarkContainerRun measures the metrics to run a container.
 func (suite *Suite) BenchmarkContainerRun(b *testing.B) {
 	assert.NoError(b, exec.Command(suite.subject, "pull", alpineImage).Run()) //nolint:gosec // testing only
 	Wrapper(b, func() {
@@ -81,6 +90,7 @@ func (suite *Suite) BenchmarkContainerRun(b *testing.B) {
 	assert.NoError(b, exec.Command(suite.subject, "rmi", "--force", alpineImage).Run()) //nolint:gosec // testing only
 }
 
+// BenchmarkImageBuild measures the metrics to build an image.
 func (suite *Suite) BenchmarkImageBuild(b *testing.B) {
 	homeDir, err := os.UserHomeDir()
 	assert.NoError(b, err)
