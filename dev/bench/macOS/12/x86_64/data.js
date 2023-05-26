97825d963005eb42742c7787b08a99efc8d97e74
github-action-benchmark <github@users.noreply.github.com>
2023-05-26 17:45:47 +0000

    add Finch Benchmark (go) benchmark result for b83cc21e6082eeff076e035dfb4cad152fb5508c

--- dev/bench/macOS/12/x86_64/data.js
+++ dev/bench/macOS/12/x86_64/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1684778049812,
+  "lastUpdate": 1685123147959,
   "repoUrl": "https://github.com/runfinch/finch",
   "entries": {
     "Finch Benchmark": [
@@ -575,6 +575,54 @@ window.BENCHMARK_DATA = {
             "name": "BenchmarkAll/BenchmarkImageBuild",
             "value": 1692006167,
             "unit": "ns/op\t         0.4404 %cpu_avg/op\t        21.43 %cpu_peak/op\t         1.692 cpu_seconds/op\t   3633152 disk_bytes/op\t   85904 B/op\t    1679 allocs/op",
+            "extra": "1 times\n12 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "cnkevin@amazon.com",
+            "name": "Kevin Li",
+            "username": "KevinLiAWS"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b83cc21e6082eeff076e035dfb4cad152fb5508c",
+          "message": "ci: Finch .pkg installer builder (#416)\n\n**Description of changes:**\r\n\r\n- This PR contains the Finch .pkg builder tool.\r\n- The .pkg builder tool can be used by GitHub action to generate and\r\nnotarize macOS .pkg installer.\r\n- The old update to S3 workflow is extended with the functionality to\r\nbuild Finch, build .pkg and test .pkg. It can be used as a scheduled job\r\nfor nightly main branch build as well to detect release issue earlier\r\nbesides cut a new release.\r\n- The old release installer workflow(test installer) is removed because\r\nits scope is moved to the integrated workflow.\r\n\r\nThis is an initial tool check in, Readme and integrated workflow to\r\nchain existing workflows to upload installer to release and publish to\r\nHomebrew will come later.\r\n\r\n**Testing done:**\r\nHave tested the updated workflow and tool on this branch, and can\r\nsuccessfully generate final installer and test it.\r\nAction test ref:\r\nhttps://github.com/runfinch/finch/actions/runs/5077161329\r\n<img width=\"1043\" alt=\"Screenshot 2023-05-24 at 11 17 16 PM\"\r\nsrc=\"https://github.com/runfinch/finch/assets/113378398/a75ab5cf-d4f0-46ac-b02a-319101aae21d\">\r\n\r\n- [X] I've reviewed the guidance in CONTRIBUTING.md\r\n\r\n\r\n#### License Acceptance\r\n\r\nBy submitting this pull request, I confirm that my contribution is made\r\nunder the terms of the Apache 2.0 license.\r\n\r\n---------\r\n\r\nSigned-off-by: Kevin Li <cnkevin@amazon.com>",
+          "timestamp": "2023-05-26T10:40:35-07:00",
+          "tree_id": "f4ee320ee326400884c15fa51a6485739e2ed39b",
+          "url": "https://github.com/runfinch/finch/commit/b83cc21e6082eeff076e035dfb4cad152fb5508c"
+        },
+        "date": 1685123147000,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkAll/BenchmarkVMInit",
+            "value": 80815974229,
+            "unit": "ns/op\t         0.3142 %cpu_avg/op\t        35.71 %cpu_peak/op\t        80.82 cpu_seconds/op\t2657595392 disk_bytes/op\t 3627016 B/op\t   76212 allocs/op",
+            "extra": "1 times\n12 procs"
+          },
+          {
+            "name": "BenchmarkAll/BenchmarkVMStart",
+            "value": 26215981608,
+            "unit": "ns/op\t         0.3804 %cpu_avg/op\t        31.25 %cpu_peak/op\t        26.22 cpu_seconds/op\t     49152 disk_bytes/op\t 1165576 B/op\t   24936 allocs/op",
+            "extra": "1 times\n12 procs"
+          },
+          {
+            "name": "BenchmarkAll/BenchmarkContainerRun",
+            "value": 454812879,
+            "unit": "ns/op\t         0.2120 %cpu_avg/op\t         9.022 %cpu_peak/op\t         0.4547 cpu_seconds/op\t         0 disk_bytes/op\t   30317 B/op\t     486 allocs/op",
+            "extra": "3 times\n12 procs"
+          },
+          {
+            "name": "BenchmarkAll/BenchmarkImageBuild",
+            "value": 1791474025,
+            "unit": "ns/op\t         0.3043 %cpu_avg/op\t        25.00 %cpu_peak/op\t         1.791 cpu_seconds/op\t  20389888 disk_bytes/op\t   89448 B/op\t    1768 allocs/op",
             "extra": "1 times\n12 procs"
           }
         ]
