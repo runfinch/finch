b83cc21e6082eeff076e035dfb4cad152fb5508c
Kevin Li <cnkevin@amazon.com>
2023-05-26 10:40:35 -0700

    ci: Finch .pkg installer builder (#416)
    **Description of changes:**
    
    - This PR contains the Finch .pkg builder tool.
    - The .pkg builder tool can be used by GitHub action to generate and
    notarize macOS .pkg installer.
    - The old update to S3 workflow is extended with the functionality to
    build Finch, build .pkg and test .pkg. It can be used as a scheduled job
    for nightly main branch build as well to detect release issue earlier
    besides cut a new release.
    - The old release installer workflow(test installer) is removed because
    its scope is moved to the integrated workflow.
    
    This is an initial tool check in, Readme and integrated workflow to
    chain existing workflows to upload installer to release and publish to
    Homebrew will come later.
    
    **Testing done:**
    Have tested the updated workflow and tool on this branch, and can
    successfully generate final installer and test it.
    Action test ref:
    https://github.com/runfinch/finch/actions/runs/5077161329
    <img width="1043" alt="Screenshot 2023-05-24 at 11 17 16 PM"
    src="https://github.com/runfinch/finch/assets/113378398/a75ab5cf-d4f0-46ac-b02a-319101aae21d">
    
    - [X] I've reviewed the guidance in CONTRIBUTING.md
    
    
    #### License Acceptance
    
    By submitting this pull request, I confirm that my contribution is made
    under the terms of the Apache 2.0 license.
    
    ---------
    
    Signed-off-by: Kevin Li <cnkevin@amazon.com>

---
+++ installer-builder/darwin/Resources/uninstall.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#check running user
+if (( $EUID != 0 )); then
+    echo "Please run as root."
+    exit
+fi
+
+echo "Finch-__VERSION__ will be REMOVED."
+while true; do
+    read -p "Do you wish to continue [Y/n]?" answer
+    [[ $answer == "y" || $answer == "Y" || $answer == "" ]] && break
+    [[ $answer == "n" || $answer == "N" ]] && exit 0
+    echo "Please answer with 'y' or 'n'"
+done
+
+echo "Application uninstalling process started"
+if [ "$$(readlink "/usr/local/bin/finch")" = "/Applications/Finch/bin/finch" ]; then sudo rm /usr/local/bin/finch; fi
+
+echo "[1/3] [DONE] Successfully deleted shortcut links"
+
+#forget from pkgutil
+pkgutil --forget "org.Finch.__VERSION__" > /dev/null 2>&1
+if [ $? -eq 0 ]
+then
+  echo "[2/3] [DONE] Successfully deleted application informations"
+else
+  echo "[2/3] [ERROR] Could not delete application informations" >&2
+fi
+
+#remove application source distribution
+[ -e "/Applications/Finch" ] && rm -rf /Applications/Finch && rm -rf /opt/finch && rm -rf /private/var/run/finch-lima && rm -rf /private/etc/sudoers.d/finch-lima
+if [ $? -eq 0 ]
+then
+  echo "[3/3] [DONE] Successfully deleted application"
+else
+  echo "[3/3] [ERROR] Could not delete application" >&2
+fi
+
+sudo pkill '^socket_vmnet'
+sudo pkill '^qemu-system-'
+
+echo "Application uninstall process finished"
+exit 0
---
+++ installer-builder/tools/notarize.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#$1: the account name
+#$2: the credential
+cd ./installer-builder/output/installer/signed/Payload
+ditto -c -k --sequesterRsrc --keepParent Finch.pkg Finch.zip
+xcrun notarytool submit Finch.zip --apple-id ${1} --password ${2} --team-id 94KV3E626L --wait
