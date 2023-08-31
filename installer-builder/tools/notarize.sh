06f464454f7d5bf5a7b61455dbe6ef1332307e0b
Austin Vazquez <55906459+austinvazquez@users.noreply.github.com>
2023-08-31 09:28:02 -0700

    ci: add ShellCheck to CI (#458)
    Issue #, if available:
    N/A
    
    *Description of changes:*
    Adds ShellCheck, a bash scripting linter, to CI as a non-critical step
    and resolves some of the trivial warnings/issues found by the tool.
    Another round of fixes is needed for some of the non-trivial
    warnings/issues.
    
    *Testing done:*
    
    
    
    - [x] I've reviewed the guidance in CONTRIBUTING.md
    
    
    #### License Acceptance
    
    By submitting this pull request, I confirm that my contribution is made
    under the terms of the Apache 2.0 license.
    
    Signed-off-by: Austin Vazquez <macedonv@amazon.com>

--- installer-builder/tools/notarize.sh
+++ installer-builder/tools/notarize.sh
@@ -2,6 +2,6 @@
 
 #$1: the account name
 #$2: the credential
-cd ./installer-builder/output/installer/signed/Payload
+cd ./installer-builder/output/installer/signed/Payload || exit
 ditto -c -k --sequesterRsrc --keepParent Finch.pkg Finch.zip
-xcrun notarytool submit Finch.zip --apple-id ${1} --password ${2} --team-id 94KV3E626L --wait
+xcrun notarytool submit Finch.zip --apple-id "${1}" --password "${2}" --team-id 94KV3E626L --wait
