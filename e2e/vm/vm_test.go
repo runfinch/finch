286db475f2744bf70725b609168b50481e9d0819
Vishwas Siravara <vsiravara@gmail.com>
2023-10-06 23:46:52 +0000

    fix: refactor reset disk and finch config test (#599)
    Issue #, if available:
    
    *Description of changes:*
    - fix path for deleting persistent disk for windows for cleanup in e2e
    tests
    - fix path for finch config e2e vm test
    *Testing done:*
    
    Yes, locally
    
    - [X] I've reviewed the guidance in CONTRIBUTING.md
    
    #### License Acceptance
    
    By submitting this pull request, I confirm that my contribution is made
    under the terms of the Apache 2.0 license.
    
    ---------
    
    Signed-off-by: Vishwas Siravara <vsiravar@gmail.com>
    Co-authored-by: Vishwas Siravara <vsiravar@gmail.com>
    Signed-off-by: Vishwas Siravara <siravara@amazon.com>

--- e2e/vm/vm_test.go
+++ e2e/vm/vm_test.go
@@ -4,9 +4,6 @@
 package vm
 
 import (
-	"errors"
-	"io/fs"
-	"os"
 	"os/exec"
 	"path/filepath"
 
@@ -32,7 +29,7 @@ var resetVM = func(o *option.Option, installed bool) string {
 		gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
 		realFinchPath, err := filepath.EvalSymlinks(path)
 		gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
-		limaConfigFilePath = filepath.Join(realFinchPath, "../../lima/data/_config/override.yaml")
+		limaConfigFilePath = filepath.Join(realFinchPath, "..", "..", "lima", "data", "_config", "override.yaml")
 	}
 	origLimaCfg := readFile(limaConfigFilePath)
 
@@ -48,25 +45,4 @@ var resetVM = func(o *option.Option, installed bool) string {
 	})
 
 	return limaConfigFilePath
-}
-
-var resetDisks = func(o *option.Option, installed bool) {
-	var dataDiskDir string
-	limaDisksPath := "lima/data/_disks/"
-	if installed {
-		path, err := exec.LookPath(e2e.InstalledTestSubject)
-		gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
-		realFinchPath, err := filepath.EvalSymlinks(path)
-		gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
-		dataDiskDir = filepath.Join(realFinchPath, "..", "..", limaDisksPath)
-	} else {
-		dataDiskDir = filepath.Join("..", "..", "_output/", limaDisksPath)
-	}
-	realDiskPath, err := os.Readlink(filepath.Join(dataDiskDir, "finch", "datadisk"))
-	if err == nil {
-		gomega.Expect(os.Remove(realDiskPath)).ShouldNot(gomega.HaveOccurred())
-	} else if !errors.Is(err, fs.ErrNotExist) {
-		gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
-	}
-	gomega.Expect(os.RemoveAll(dataDiskDir)).ShouldNot(gomega.HaveOccurred())
 }
