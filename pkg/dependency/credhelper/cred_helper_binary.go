939ba77601a9d579bbf406264b9119e44982be83
Justin Alvarez <alvajus@amazon.com>
2023-10-31 16:12:35 -0400

    update tests
    Signed-off-by: Justin Alvarez <alvajus@amazon.com>

--- pkg/dependency/credhelper/cred_helper_binary.go
+++ pkg/dependency/credhelper/cred_helper_binary.go
@@ -103,6 +103,10 @@ func updateConfigFile(bin *credhelperbin) error {
 	return nil
 }
 
+func (bin *credhelperbin) binaryInstalled() (bool, error) {
+	return binaryInstalled(bin)
+}
+
 func (bin *credhelperbin) configFileInstalled() (bool, error) {
 	cfgPath := fmt.Sprintf("%s%s", bin.hcfg.finchPath, "config.json")
 	binCfgName := bin.credHelperConfigName()
@@ -145,39 +149,10 @@ func (bin *credhelperbin) fullInstallPath() string {
 // Installed checks if the credential helper already exists in the specified
 // folder and checks if the hash of the installed binary is correct.
 func (bin *credhelperbin) Installed() bool {
-	dirExists, err := afero.DirExists(bin.fs, bin.hcfg.installFolder)
-	if err != nil {
-		bin.l.Errorf("failed to get status of credential helper directory: %v", err)
-		return false
-	}
-	if !dirExists {
-		return false
-	}
-	fileExists, err := afero.Exists(bin.fs, bin.fullInstallPath())
-	if err != nil {
-		bin.l.Errorf("failed to get status of credential helper binary: %v", err)
-		return false
-	}
-	if !fileExists {
-		return false
-	}
-	file, err := bin.fs.Open(bin.fullInstallPath())
-	if err != nil {
+	if binInstalled, err := bin.binaryInstalled(); err != nil {
 		bin.l.Error(err)
 		return false
-	}
-	defer file.Close() //nolint:errcheck // closing the file
-	hash, err := digest.FromReader(file)
-	if err != nil {
-		bin.l.Error(err)
-		return false
-	}
-	if hash.String() != bin.hcfg.hash {
-		bin.l.Info("Hash of the installed credential helper binary does not match")
-		err := bin.fs.Remove(bin.fullInstallPath())
-		if err != nil {
-			bin.l.Error(err)
-		}
+	} else if !binInstalled {
 		return false
 	}
 
@@ -193,39 +168,102 @@ func (bin *credhelperbin) Installed() bool {
 
 // Install installs and configures the specified credential helper.
 func (bin *credhelperbin) Install() error {
-	if bin.helper == "" {
-		return nil
-	}
-	if bin.helper != bin.credHelperConfigName() {
-		return nil
-	}
-	credHelperName := strings.ReplaceAll(bin.credHelperConfigName(), "-login", "")
-	bin.l.Infof("Installing %s credential helper", credHelperName)
-	mkdirCmd := bin.cmdCreator.Create("mkdir", "-p", bin.hcfg.installFolder)
-	_, err := mkdirCmd.Output()
-	if err != nil {
-		return fmt.Errorf("error creating installation directory %s, err: %w", bin.hcfg.installFolder, err)
-	}
-
-	curlCmd := bin.cmdCreator.Create("curl", "--retry", "5", "--retry-max-time", "30", "--url",
-		bin.hcfg.credHelperURL, "--output", bin.fullInstallPath())
-
-	_, err = curlCmd.Output()
-	if err != nil {
-		return fmt.Errorf("error installing binary %s, err: %w", bin.hcfg.binaryName, err)
-	}
-	err = bin.fs.Chmod(bin.fullInstallPath(), 0o755)
+	binInstalled, err := bin.binaryInstalled()
 	if err != nil {
 		return err
 	}
-	err = updateConfigFile(bin)
+
+	if !binInstalled {
+		if bin.helper == "" {
+			return nil
+		}
+		if bin.helper != bin.credHelperConfigName() {
+			return nil
+		}
+		credHelperName := strings.ReplaceAll(bin.credHelperConfigName(), "-login", "")
+		bin.l.Infof("Installing %s credential helper", credHelperName)
+		mkdirCmd := bin.cmdCreator.Create("mkdir", "-p", bin.hcfg.installFolder)
+		if _, err := mkdirCmd.Output(); err != nil {
+			return fmt.Errorf("error creating installation directory %s, err: %w", bin.hcfg.installFolder, err)
+		}
+
+		curlCmd := bin.cmdCreator.Create(
+			"curl",
+			"--retry",
+			"5",
+			"--retry-max-time",
+			"30",
+			"--url",
+			bin.hcfg.credHelperURL,
+			"--output",
+			bin.fullInstallPath(),
+		)
+
+		if _, err = curlCmd.Output(); err != nil {
+			return fmt.Errorf("error installing binary %s, err: %w", bin.hcfg.binaryName, err)
+		}
+		if err := bin.fs.Chmod(bin.fullInstallPath(), 0o755); err != nil {
+			return err
+		}
+	}
+
+	cfgInstalled, err := bin.configFileInstalled()
 	if err != nil {
 		return err
 	}
+
+	if !cfgInstalled {
+		if err := updateConfigFile(bin); err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
 
 // RequiresRoot returns whether the installation of the binary needs root permissions.
 func (bin *credhelperbin) RequiresRoot() bool {
 	return false
+}
+
+// Using a var function allows overriding during testing.
+// This is needed because the curl command directly outputs to a file, but binaryInstalled deletes
+// any incorrect file that might exist at the fullInstallPath.
+// This means that the mocking method that is typically used to mock filesystem will get the file it
+// creates deleted, and then, since cmd.Output() is what is writing the new/correct file, and there's
+// no opportunity to mock it or insert the file after it runs, the code that expects the file to exist
+// then errors out because it was deleted by binaryInstalled.
+var binaryInstalled = func(bin *credhelperbin) (bool, error) {
+	dirExists, err := afero.DirExists(bin.fs, bin.hcfg.installFolder)
+	if err != nil {
+		return false, fmt.Errorf("failed to get status of credential helper directory: %w", err)
+	}
+	if !dirExists {
+		return false, nil
+	}
+	fileExists, err := afero.Exists(bin.fs, bin.fullInstallPath())
+	if err != nil {
+		return false, fmt.Errorf("failed to get status of credential helper binary: %w", err)
+	}
+	if !fileExists {
+		return false, nil
+	}
+	file, err := bin.fs.Open(bin.fullInstallPath())
+	if err != nil {
+		return false, fmt.Errorf("failed to open cred helper binary: %w", err)
+	}
+	defer file.Close() //nolint:errcheck // closing the file
+	hash, err := digest.FromReader(file)
+	if err != nil {
+		return false, fmt.Errorf("failed to get cred helper binary hash: %w", err)
+	}
+	if hash.String() != bin.hcfg.hash {
+		bin.l.Info("Hash of the installed credential helper binary does not match")
+		if err := bin.fs.Remove(bin.fullInstallPath()); err != nil {
+			return false, fmt.Errorf("failed to remove mismatched cred helper binary: %w", err)
+		}
+		return false, nil
+	}
+
+	return true, nil
 }
