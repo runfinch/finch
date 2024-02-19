a96788bbce5e9a6af805650a1c21d1c60e04fa68
Justin <pendo324@users.noreply.github.com>
2024-02-19 14:42:11 -0500

    fix: stop Finch on uninstall (#818)
    Issue #, if available: Fixes #794
    
    *Description of changes:*
    - The uninstall script runs on upgrade, and this makes the uninstall
    script stop/remove Finch's VM
    
    *Testing done:*
    
    
    
    - [x] I've reviewed the guidance in CONTRIBUTING.md
    
    
    #### License Acceptance
    
    By submitting this pull request, I confirm that my contribution is made
    under the terms of the Apache 2.0 license.
    
    Signed-off-by: Justin Alvarez <alvajus@amazon.com>

--- msi-builder/uninstall.bat
+++ msi-builder/uninstall.bat
@@ -1,5 +1,13 @@
 @echo off
 SET InstallDir=%~1
 
+:: Stop and remove any running instance
+finch.exe vm stop -f
+finch.exe vm remove -f
+
+:: Just in case
+wsl --terminate lima-finch
+wsl --unregister lima-finch
+
 :: Delete files and directories if they exist
 if exist "%InstallDir%\lima\" rmdir /s /q "%InstallDir%\lima\"
