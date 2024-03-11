4b04d680f70be27393c586d7728b5b367b4d0ca0
Justin Alvarez <alvajus@amazon.com>
2024-03-11 18:51:41 -0400

    test: add strict version checks for packages
    Signed-off-by: Justin Alvarez <alvajus@amazon.com>

--- e2e/vm/version_test.go
+++ e2e/vm/version_test.go
@@ -4,8 +4,9 @@
 package vm
 
 import (
-	"fmt"
+	"bytes"
 	"strings"
+	"text/template"
 
 	"github.com/onsi/ginkgo/v2"
 	"github.com/onsi/gomega"
@@ -15,31 +16,61 @@ import (
 	"github.com/runfinch/finch/pkg/version"
 )
 
+const (
+	nerdctlVersion    = "v1.7.3"
+	buildKitVersion   = "v0.12.5"
+	containerdVersion = "v1.7.13"
+	runcVersion       = "1.1.12"
+)
+
+type Versions struct {
+	FinchVersion      string
+	FinchCommit       string
+	NerdctlVersion    string
+	BuildKitVersion   string
+	ContainerdVersion string
+	RuncVersion       string
+}
+
+var versions = Versions{
+	FinchVersion:      strings.ReplaceAll(version.Version, ".", "\\."),
+	FinchCommit:       strings.ReplaceAll(version.GitCommit, ".", "\\."),
+	NerdctlVersion:    strings.ReplaceAll(nerdctlVersion, ".", "\\."),
+	BuildKitVersion:   strings.ReplaceAll(buildKitVersion, ".", "\\."),
+	ContainerdVersion: strings.ReplaceAll(containerdVersion, ".", "\\."),
+	RuncVersion:       strings.ReplaceAll(runcVersion, ".", "\\."),
+}
+
 // Checks finch version.
 var testVersion = func(o *option.Option) {
 	ginkgo.Describe("Check finch version", func() {
 		ginkgo.It("Should print finch version information", func() {
-			// StdoutStr is not used because it trims both leading and trailing spaces,
-			// while we want an exact match here.
-			gomega.Expect(string(command.Stdout(o, "version"))).Should(gomega.MatchRegexp(fmt.Sprintf(`Client:
- Version:	%s
+			tmpl, err := template.New("versionTemplate").Parse(`Client:
+ Version:	{{ .FinchVersion }}
  OS\/Arch:	[A-Za-z0-9]+\/[A-Za-z0-9]+
- GitCommit:	%s
+ GitCommit:	{{ .FinchCommit }}
  nerdctl:
-  Version:	v[0-9]+\.[0-9]+\.[0-9]+
+  Version:	{{ .NerdctlVersion }}
   GitCommit:	[a-z0-9]{40}
  buildctl:
-  Version:	v[0-9]+\.[0-9]+\.[0-9]+
+  Version:	{{ .BuildKitVersion }}
   GitCommit:	[a-z0-9]{40}
 
 Server:
  containerd:
-  Version:	v[0-9]+\.[0-9]+\.[0-9]+
+  Version:	{{ .ContainerdVersion }}
   GitCommit:	[a-z0-9]{40}
  runc:
-  Version:	[0-9]+\.[0-9]+\.[0-9]+
+  Version:	{{ .RuncVersion }}
   GitCommit:	v[0-9]+\.[0-9]+\.[0-9]+(-[0-9]+-g[a-z0-9]{8})?
-`, strings.ReplaceAll(version.Version, ".", "\\."), version.GitCommit)))
+`)
+			gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
+			var versionMatcher bytes.Buffer
+			err = tmpl.Execute(&versionMatcher, versions)
+			gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
+			// command.StdoutStr is not used because it trims both leading and trailing spaces,
+			// while we want an exact match here.
+			gomega.Expect(string(command.Stdout(o, "version"))).Should(gomega.MatchRegexp(versionMatcher.String()))
 		})
 	})
 }
