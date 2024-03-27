2fe52c032ae0ec5614aa1cfeda02fdc3d56bd791
github-action-benchmark <github@users.noreply.github.com>
2024-03-27 15:09:42 +0000

    add Finch Benchmark (go) benchmark result for 4167d3dd61865b22bca9e7c54f839ba1d90138bf

--- dev/bench/macOS/13/arm64/data.js
+++ dev/bench/macOS/13/arm64/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1711478824877,
+  "lastUpdate": 1711552182288,
   "repoUrl": "https://github.com/runfinch/finch",
   "entries": {
     "Finch Benchmark": [
@@ -38068,6 +38068,174 @@ window.BENCHMARK_DATA = {
           {
             "name": "BenchmarkAll/BenchmarkContainerRun - allocs/op",
             "value": 317,
+            "unit": "allocs/op",
+            "extra": "4 times\n8 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "4167d3dd61865b22bca9e7c54f839ba1d90138bf",
+          "message": "build(deps): Bump github.com/docker/cli from 25.0.4+incompatible to 26.0.0+incompatible (#867)\n\nBumps [github.com/docker/cli](https://github.com/docker/cli) from\r\n25.0.4+incompatible to 26.0.0+incompatible.\r\n<details>\r\n<summary>Commits</summary>\r\n<ul>\r\n<li><a\r\nhref=\"https://github.com/docker/cli/commit/2ae903e86cab51f694c819721cdfdf5eec693720\"><code>2ae903e</code></a>\r\nMerge pull request <a\r\nhref=\"https://redirect.github.com/docker/cli/issues/4929\">#4929</a> from\r\ndvdksn/privileged-flag</li>\r\n<li><a\r\nhref=\"https://github.com/docker/cli/commit/5931a2f592c91aa9fa87a9a044512cb7d736427c\"><code>5931a2f</code></a>\r\nMerge pull request <a\r\nhref=\"https://redirect.github.com/docker/cli/issues/4956\">#4956</a> from\r\nvvoland/vendor-docker-26.0.0-dev</li>\r\n<li><a\r\nhref=\"https://github.com/docker/cli/commit/ed9dd75575254b9b28d0fc0d7b54b2491acc38e1\"><code>ed9dd75</code></a>\r\nMerge pull request <a\r\nhref=\"https://redirect.github.com/docker/cli/issues/4949\">#4949</a> from\r\nakerouanton/remove-short-cid-alias</li>\r\n<li><a\r\nhref=\"https://github.com/docker/cli/commit/69575f6175a75bab1377c24abcb57e3d56357087\"><code>69575f6</code></a>\r\nvendor: github.com/docker/docker 8b79278316b5 (master)</li>\r\n<li><a\r\nhref=\"https://github.com/docker/cli/commit/fa84cfd8020a4c221ab97da1c11507c1c5e552fd\"><code>fa84cfd</code></a>\r\nMerge pull request <a\r\nhref=\"https://redirect.github.com/docker/cli/issues/4950\">#4950</a> from\r\nvvoland/vendor-docker-26.0.0-rc3-dev</li>\r\n<li><a\r\nhref=\"https://github.com/docker/cli/commit/b70a26deaf1c6e157eec0569761f8d374c94758e\"><code>b70a26d</code></a>\r\nvendor: github.com/docker/docker 330d777c53fb (v26.0.0-rc3-dev)</li>\r\n<li><a\r\nhref=\"https://github.com/docker/cli/commit/23563728bc6f695e7f3d21026fefe64888bdd552\"><code>2356372</code></a>\r\nMerge pull request <a\r\nhref=\"https://redirect.github.com/docker/cli/issues/4944\">#4944</a> from\r\nthaJeztah/update_engine</li>\r\n<li><a\r\nhref=\"https://github.com/docker/cli/commit/ea3201c575a0210c7093bd43b3cb8cc5c42f8615\"><code>ea3201c</code></a>\r\nMerge pull request <a\r\nhref=\"https://redirect.github.com/docker/cli/issues/4945\">#4945</a> from\r\nthaJeztah/remove_gocompat</li>\r\n<li><a\r\nhref=\"https://github.com/docker/cli/commit/c050bf0909f34f3f453b30c5f83dc173ddc2add6\"><code>c050bf0</code></a>\r\nMerge pull request <a\r\nhref=\"https://redirect.github.com/docker/cli/issues/4946\">#4946</a> from\r\nthaJeztah/bump_gofumpt</li>\r\n<li><a\r\nhref=\"https://github.com/docker/cli/commit/4eef4afbf4a5bd85f9d32dafa70cafa0061bf5b4\"><code>4eef4af</code></a>\r\nMerge pull request <a\r\nhref=\"https://redirect.github.com/docker/cli/issues/4893\">#4893</a> from\r\nvvoland/deprecate-container</li>\r\n<li>Additional commits viewable in <a\r\nhref=\"https://github.com/docker/cli/compare/v25.0.4...v26.0.0\">compare\r\nview</a></li>\r\n</ul>\r\n</details>\r\n<br />\r\n\r\n\r\n[![Dependabot compatibility\r\nscore](https://dependabot-badges.githubapp.com/badges/compatibility_score?dependency-name=github.com/docker/cli&package-manager=go_modules&previous-version=25.0.4+incompatible&new-version=26.0.0+incompatible)](https://docs.github.com/en/github/managing-security-vulnerabilities/about-dependabot-security-updates#about-compatibility-scores)\r\n\r\nDependabot will resolve any conflicts with this PR as long as you don't\r\nalter it yourself. You can also trigger a rebase manually by commenting\r\n`@dependabot rebase`.\r\n\r\n[//]: # (dependabot-automerge-start)\r\n[//]: # (dependabot-automerge-end)\r\n\r\n---\r\n\r\n<details>\r\n<summary>Dependabot commands and options</summary>\r\n<br />\r\n\r\nYou can trigger Dependabot actions by commenting on this PR:\r\n- `@dependabot rebase` will rebase this PR\r\n- `@dependabot recreate` will recreate this PR, overwriting any edits\r\nthat have been made to it\r\n- `@dependabot merge` will merge this PR after your CI passes on it\r\n- `@dependabot squash and merge` will squash and merge this PR after\r\nyour CI passes on it\r\n- `@dependabot cancel merge` will cancel a previously requested merge\r\nand block automerging\r\n- `@dependabot reopen` will reopen this PR if it is closed\r\n- `@dependabot close` will close this PR and stop Dependabot recreating\r\nit. You can achieve the same result by closing it manually\r\n- `@dependabot show <dependency name> ignore conditions` will show all\r\nof the ignore conditions of the specified dependency\r\n- `@dependabot ignore this major version` will close this PR and stop\r\nDependabot creating any more for this major version (unless you reopen\r\nthe PR or upgrade to it yourself)\r\n- `@dependabot ignore this minor version` will close this PR and stop\r\nDependabot creating any more for this minor version (unless you reopen\r\nthe PR or upgrade to it yourself)\r\n- `@dependabot ignore this dependency` will close this PR and stop\r\nDependabot creating any more for this dependency (unless you reopen the\r\nPR or upgrade to it yourself)\r\n\r\n\r\n</details>\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2024-03-27T11:05:58-04:00",
+          "tree_id": "3b033ad640fce603ed827f16985c01674450df9c",
+          "url": "https://github.com/runfinch/finch/commit/4167d3dd61865b22bca9e7c54f839ba1d90138bf"
+        },
+        "date": 1711552180401,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkAll/BenchmarkVMInit",
+            "value": 37502543084,
+            "unit": "ns/op\t         0.5211 %cpu_avg/op\t        55.56 %cpu_peak/op\t        37.50 cpu_seconds/op\t1193881600 disk_bytes/op\t 1702512 B/op\t   35541 allocs/op",
+            "extra": "1 times\n8 procs"
+          },
+          {
+            "name": "BenchmarkAll/BenchmarkVMInit - ns/op",
+            "value": 37502543084,
+            "unit": "ns/op",
+            "extra": "1 times\n8 procs"
+          },
+          {
+            "name": "BenchmarkAll/BenchmarkVMInit - %cpu_avg/op",
+            "value": 0.5211,
+            "unit": "%cpu_avg/op",
+            "extra": "1 times\n8 procs"
+          },
+          {
+            "name": "BenchmarkAll/BenchmarkVMInit - %cpu_peak/op",
+            "value": 55.56,
+            "unit": "%cpu_peak/op",
+            "extra": "1 times\n8 procs"
+          },
+          {
+            "name": "BenchmarkAll/BenchmarkVMInit - cpu_seconds/op",
+            "value": 37.5,
+            "unit": "cpu_seconds/op",
+            "extra": "1 times\n8 procs"
+          },
+          {
+            "name": "BenchmarkAll/BenchmarkVMInit - disk_bytes/op",
+            "value": 1193881600,
+            "unit": "disk_bytes/op",
+            "extra": "1 times\n8 procs"
+          },
+          {
+            "name": "BenchmarkAll/BenchmarkVMInit - B/op",
+            "value": 1702512,
+            "unit": "B/op",
+            "extra": "1 times\n8 procs"
+          },
+          {
+            "name": "BenchmarkAll/BenchmarkVMInit - allocs/op",
+            "value": 35541,
+            "unit": "allocs/op",
+            "extra": "1 times\n8 procs"
+          },
+          {
+            "name": "BenchmarkAll/BenchmarkVMStart",
+            "value": 20482187250,
+            "unit": "ns/op\t         0.5927 %cpu_avg/op\t        45.45 %cpu_peak/op\t        20.48 cpu_seconds/op\t  19496960 disk_bytes/op\t  922848 B/op\t   19426 allocs/op",
+            "extra": "1 times\n8 procs"
+          },
+          {
+            "name": "BenchmarkAll/BenchmarkVMStart - ns/op",
+            "value": 20482187250,
+            "unit": "ns/op",
+            "extra": "1 times\n8 procs"
+          },
+          {
+            "name": "BenchmarkAll/BenchmarkVMStart - %cpu_avg/op",
+            "value": 0.5927,
+            "unit": "%cpu_avg/op",
+            "extra": "1 times\n8 procs"
+          },
+          {
+            "name": "BenchmarkAll/BenchmarkVMStart - %cpu_peak/op",
+            "value": 45.45,
+            "unit": "%cpu_peak/op",
+            "extra": "1 times\n8 procs"
+          },
+          {
+            "name": "BenchmarkAll/BenchmarkVMStart - cpu_seconds/op",
+            "value": 20.48,
+            "unit": "cpu_seconds/op",
+            "extra": "1 times\n8 procs"
+          },
+          {
+            "name": "BenchmarkAll/BenchmarkVMStart - disk_bytes/op",
+            "value": 19496960,
+            "unit": "disk_bytes/op",
+            "extra": "1 times\n8 procs"
+          },
+          {
+            "name": "BenchmarkAll/BenchmarkVMStart - B/op",
+            "value": 922848,
+            "unit": "B/op",
+            "extra": "1 times\n8 procs"
+          },
+          {
+            "name": "BenchmarkAll/BenchmarkVMStart - allocs/op",
+            "value": 19426,
+            "unit": "allocs/op",
+            "extra": "1 times\n8 procs"
+          },
+          {
+            "name": "BenchmarkAll/BenchmarkContainerRun",
+            "value": 271501552,
+            "unit": "ns/op\t         1.277 %cpu_avg/op\t        19.64 %cpu_peak/op\t         0.2714 cpu_seconds/op\t  -1978368 disk_bytes/op\t   22416 B/op\t     305 allocs/op",
+            "extra": "4 times\n8 procs"
+          },
+          {
+            "name": "BenchmarkAll/BenchmarkContainerRun - ns/op",
+            "value": 271501552,
+            "unit": "ns/op",
+            "extra": "4 times\n8 procs"
+          },
+          {
+            "name": "BenchmarkAll/BenchmarkContainerRun - %cpu_avg/op",
+            "value": 1.277,
+            "unit": "%cpu_avg/op",
+            "extra": "4 times\n8 procs"
+          },
+          {
+            "name": "BenchmarkAll/BenchmarkContainerRun - %cpu_peak/op",
+            "value": 19.64,
+            "unit": "%cpu_peak/op",
+            "extra": "4 times\n8 procs"
+          },
+          {
+            "name": "BenchmarkAll/BenchmarkContainerRun - cpu_seconds/op",
+            "value": 0.2714,
+            "unit": "cpu_seconds/op",
+            "extra": "4 times\n8 procs"
+          },
+          {
+            "name": "BenchmarkAll/BenchmarkContainerRun - disk_bytes/op",
+            "value": -1978368,
+            "unit": "disk_bytes/op",
+            "extra": "4 times\n8 procs"
+          },
+          {
+            "name": "BenchmarkAll/BenchmarkContainerRun - B/op",
+            "value": 22416,
+            "unit": "B/op",
+            "extra": "4 times\n8 procs"
+          },
+          {
+            "name": "BenchmarkAll/BenchmarkContainerRun - allocs/op",
+            "value": 305,
             "unit": "allocs/op",
             "extra": "4 times\n8 procs"
           }
