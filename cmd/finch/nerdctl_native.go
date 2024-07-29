dc6688bc44bdd67f418926e2dd6b5f840b11f792
Justin Alvarez <alvajus@amazon.com>
2024-07-29 12:15:34 -0400

    remove ensureCredentials check in native mode
    Signed-off-by: Justin Alvarez <alvajus@amazon.com>

--- cmd/finch/nerdctl_native.go
+++ cmd/finch/nerdctl_native.go
@@ -13,29 +13,15 @@ import (
 )
 
 func (nc *nerdctlCommand) run(cmdName string, args []string) error {
-	var additionalEnv []string
-	switch cmdName {
-	case "image":
-		if slices.Contains(args, "build") || slices.Contains(args, "pull") || slices.Contains(args, "push") {
-			ensureRemoteCredentials(nc.fc, nc.ecc, &additionalEnv, nc.logger)
-		}
-	case "container":
-		if slices.Contains(args, "run") {
-			ensureRemoteCredentials(nc.fc, nc.ecc, &additionalEnv, nc.logger)
-		}
-	case "build", "pull", "push", "run":
-		ensureRemoteCredentials(nc.fc, nc.ecc, &additionalEnv, nc.logger)
-	}
-
-	if nc.shouldReplaceForHelp(cmdName, args) {
-		return nc.ncc.RunWithReplacingStdout([]command.Replacement{{Source: "nerdctl", Target: "finch"}}, args...)
-	}
-
 	// eat the debug arg, and set the log level to avoid nerdctl parsing this flag
 	dbgIdx := slices.Index(args, "--debug")
 	if dbgIdx >= 0 {
 		args = append(args[:dbgIdx], args[dbgIdx+1:]...)
 		nc.logger.SetLevel(flog.Debug)
+	}
+
+	if nc.shouldReplaceForHelp(cmdName, args) {
+		return nc.ncc.RunWithReplacingStdout([]command.Replacement{{Source: "nerdctl", Target: "finch"}}, args...)
 	}
 
 	return nc.ncc.Create(append([]string{cmdName}, args...)...).Run()
