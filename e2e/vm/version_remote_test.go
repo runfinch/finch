5b20e157575cd19166c34c087e70ea967d4e73e0
Justin Alvarez <alvajus@amazon.com>
2025-02-10 19:08:58 -0500

    update the rest of the dependency versions
    Signed-off-by: Justin Alvarez <alvajus@amazon.com>

--- e2e/vm/version_remote_test.go
+++ e2e/vm/version_remote_test.go
@@ -20,9 +20,9 @@ import (
 
 const (
 	nerdctlVersion    = "v2.0.3"
-	buildKitVersion   = "v0.18.1"
-	containerdVersion = "v2.0.0"
-	runcVersion       = "1.2.2"
+	buildKitVersion   = "v0.19.0"
+	containerdVersion = "v2.0.2"
+	runcVersion       = "1.2.4"
 )
 
 type Versions struct {
