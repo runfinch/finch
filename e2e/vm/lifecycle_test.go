fe19eef4208a00d08c0e926793cb6f6ea3181145
Subhransu <47723536+Shubhranshu153@users.noreply.github.com>
2025-08-18 14:40:54 -0700

    feat: optimize vm lifecycle tests (#1508)
    Signed-off-by: Shubhranshu Mahapatra <shubhum@amazon.com>

--- e2e/vm/lifecycle_test.go
+++ e2e/vm/lifecycle_test.go
@@ -6,39 +6,15 @@
 package vm
 
 import (
-	"runtime"
-	"time"
-
 	"github.com/onsi/ginkgo/v2"
 	"github.com/onsi/gomega"
 	"github.com/runfinch/common-tests/command"
 	"github.com/runfinch/common-tests/option"
 )
 
-var restoreVM = func(o *option.Option) {
-	origFinchCfg := readFile(finchConfigFilePath)
-	writeFile(finchConfigFilePath, origFinchCfg)
-	command.New(o, virtualMachineRootCmd, "stop", "-f").WithoutCheckingExitCode().WithTimeoutInSeconds(20).Run()
-	time.Sleep(1 * time.Second)
-	command.New(o, virtualMachineRootCmd, "remove", "-f").WithoutCheckingExitCode().WithTimeoutInSeconds(10).Run()
-	time.Sleep(1 * time.Second)
-	if runtime.GOOS == "windows" {
-		// clean up iptables
-		//nolint:lll // link to explanation
-		// https://docs.rancherdesktop.io/troubleshooting-tips/#q-how-do-i-fix-fata0005-subnet-1040024-overlaps-with-other-one-on-this-address-space-when-running-a-container-using-nerdctl-run
-		gomega.Expect(shutdownWSL()).Should(gomega.BeNil())
-	}
-	time.Sleep(1 * time.Second)
-	command.New(o, virtualMachineRootCmd, "init").WithoutCheckingExitCode().WithTimeoutInSeconds(160).Run()
-}
-
 var testVMLifecycle = func(o *option.Option) {
-	ginkgo.Describe("virtual machine lifecycle", ginkgo.Serial, func() {
+	ginkgo.Describe("virtual machine lifecycle", ginkgo.Serial, ginkgo.Ordered, func() {
 		ginkgo.Describe("when the virtual machine is in running status", func() {
-			ginkgo.AfterEach(func() {
-				restoreVM(o)
-			})
-
 			ginkgo.It("should fail to init/remove the virtual machine", func() {
 				command.New(o, virtualMachineRootCmd, "init").WithoutSuccessfulExit().Run()
 				command.New(o, virtualMachineRootCmd, "remove").WithoutSuccessfulExit().Run()
@@ -48,33 +24,10 @@ var testVMLifecycle = func(o *option.Option) {
 				gomega.Expect(command.StdoutStr(o, virtualMachineRootCmd, "status")).To(gomega.Equal("Running"))
 			})
 
-			ginkgo.It("should be able to force stop the virtual machine", func() {
-				command.Run(o, "images")
-				command.New(o, virtualMachineRootCmd, "stop", "--force").WithTimeoutInSeconds(180).Run()
-				command.RunWithoutSuccessfulExit(o, "images")
-				command.New(o, virtualMachineRootCmd, "start").WithTimeoutInSeconds(240).Run()
-			})
-
-			ginkgo.It("should be able to force remove the virtual machine", func() {
-				command.Run(o, "images")
-				command.New(o, virtualMachineRootCmd, "remove", "--force").WithTimeoutInSeconds(160).Run()
-				command.RunWithoutSuccessfulExit(o, "images")
-			})
-
 			ginkgo.It("should be able to stop the virtual machine", func() {
 				command.Run(o, "images")
 				command.New(o, virtualMachineRootCmd, "stop").WithTimeoutInSeconds(90).Run()
 				command.RunWithoutSuccessfulExit(o, "images")
-			})
-		})
-
-		ginkgo.Describe("when the virtual machine is in stopped status", func() {
-			ginkgo.BeforeEach(func() {
-				command.New(o, virtualMachineRootCmd, "stop", "--force").WithTimeoutInSeconds(90).Run()
-			})
-
-			ginkgo.AfterEach(func() {
-				restoreVM(o)
 			})
 
 			ginkgo.It("should fail to init/stop", func() {
@@ -92,24 +45,16 @@ var testVMLifecycle = func(o *option.Option) {
 				command.New(o, virtualMachineRootCmd, "stop").WithTimeoutInSeconds(90).Run()
 			})
 
+			ginkgo.It("should be able to force stop the virtual machine after restarting", func() {
+				command.New(o, virtualMachineRootCmd, "start").WithTimeoutInSeconds(240).Run()
+				command.Run(o, "images")
+				command.New(o, virtualMachineRootCmd, "stop", "--force").WithTimeoutInSeconds(180).Run()
+				command.RunWithoutSuccessfulExit(o, "images")
+			})
+
 			ginkgo.It("should be able to remove the virtual machine", func() {
 				command.New(o, virtualMachineRootCmd, "remove").WithTimeoutInSeconds(160).Run()
 				command.RunWithoutSuccessfulExit(o, "images")
-			})
-
-			ginkgo.It("should be able to force remove the virtual machine", func() {
-				command.New(o, virtualMachineRootCmd, "remove", "--force").WithTimeoutInSeconds(160).Run()
-				command.RunWithoutSuccessfulExit(o, "images")
-			})
-		})
-
-		ginkgo.Describe("when the virtual machine instance does not exist", func() {
-			ginkgo.BeforeEach(func() {
-				command.New(o, virtualMachineRootCmd, "remove", "--force").WithTimeoutInSeconds(160).Run()
-			})
-
-			ginkgo.AfterEach(func() {
-				restoreVM(o)
 			})
 
 			ginkgo.It("should fail to start/stop", func() {
@@ -119,11 +64,6 @@ var testVMLifecycle = func(o *option.Option) {
 
 			ginkgo.It("should get nonexistent status", func() {
 				gomega.Expect(command.StdoutStr(o, virtualMachineRootCmd, "status")).To(gomega.Equal("Nonexistent"))
-			})
-
-			ginkgo.It("should be able to init", func() {
-				command.New(o, virtualMachineRootCmd, "init").WithTimeoutInSeconds(160).Run()
-				command.Run(o, "images")
 			})
 		})
 	})
