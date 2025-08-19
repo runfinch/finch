558c80db8f4de858a727e4d5d387651d3cfc6598
Subhransu <47723536+Shubhranshu153@users.noreply.github.com>
2025-08-19 09:32:32 -0700

    feat: add force remove test and re-arrange test based on state changes (#1509)
    Signed-off-by: Shubhranshu Mahapatra <shubhum@amazon.com>

--- e2e/vm/lifecycle_test.go
+++ e2e/vm/lifecycle_test.go
@@ -15,46 +15,39 @@ import (
 var testVMLifecycle = func(o *option.Option) {
 	ginkgo.Describe("virtual machine lifecycle", ginkgo.Serial, ginkgo.Ordered, func() {
 		ginkgo.Describe("when the virtual machine is in running status", func() {
+			// Test Running -> Stopped
+			ginkgo.It("should get running status", func() {
+				gomega.Expect(command.StdoutStr(o, virtualMachineRootCmd, "status")).To(gomega.Equal("Running"))
+				command.Run(o, "images")
+			})
+
 			ginkgo.It("should fail to init/remove the virtual machine", func() {
 				command.New(o, virtualMachineRootCmd, "init").WithoutSuccessfulExit().Run()
 				command.New(o, virtualMachineRootCmd, "remove").WithoutSuccessfulExit().Run()
 			})
 
-			ginkgo.It("should get running status", func() {
-				gomega.Expect(command.StdoutStr(o, virtualMachineRootCmd, "status")).To(gomega.Equal("Running"))
-			})
-
 			ginkgo.It("should be able to stop the virtual machine", func() {
-				command.Run(o, "images")
+				gomega.Expect(command.StdoutStr(o, virtualMachineRootCmd, "status")).To(gomega.Equal("Running"))
 				command.New(o, virtualMachineRootCmd, "stop").WithTimeoutInSeconds(90).Run()
-				command.RunWithoutSuccessfulExit(o, "images")
+				gomega.Expect(command.StdoutStr(o, virtualMachineRootCmd, "status")).To(gomega.Equal("Stopped"))
 			})
 
-			ginkgo.It("should fail to init/stop", func() {
+			ginkgo.It("should fail to stop/init", func() {
 				command.New(o, virtualMachineRootCmd, "stop").WithoutSuccessfulExit().Run()
 				command.New(o, virtualMachineRootCmd, "init").WithoutSuccessfulExit().Run()
 			})
 
-			ginkgo.It("should get stopped status", func() {
-				gomega.Expect(command.StdoutStr(o, virtualMachineRootCmd, "status")).To(gomega.Equal("Stopped"))
-			})
-
+			// Test Stopped -> Running
 			ginkgo.It("should be able to start the virtual machine", func() {
 				command.New(o, virtualMachineRootCmd, "start").WithTimeoutInSeconds(240).Run()
+				gomega.Expect(command.StdoutStr(o, virtualMachineRootCmd, "status")).To(gomega.Equal("Running"))
 				command.Run(o, "images")
-				command.New(o, virtualMachineRootCmd, "stop").WithTimeoutInSeconds(90).Run()
 			})
 
-			ginkgo.It("should be able to force stop the virtual machine after restarting", func() {
-				command.New(o, virtualMachineRootCmd, "start").WithTimeoutInSeconds(240).Run()
-				command.Run(o, "images")
-				command.New(o, virtualMachineRootCmd, "stop", "--force").WithTimeoutInSeconds(180).Run()
-				command.RunWithoutSuccessfulExit(o, "images")
-			})
-
+			// Test Running -> NonExistent
 			ginkgo.It("should be able to remove the virtual machine", func() {
-				command.New(o, virtualMachineRootCmd, "remove").WithTimeoutInSeconds(160).Run()
-				command.RunWithoutSuccessfulExit(o, "images")
+				command.New(o, virtualMachineRootCmd, "remove", "-f").WithTimeoutInSeconds(90).Run()
+				gomega.Expect(command.StdoutStr(o, virtualMachineRootCmd, "status")).To(gomega.Equal("Nonexistent"))
 			})
 
 			ginkgo.It("should fail to start/stop", func() {
@@ -62,9 +55,17 @@ var testVMLifecycle = func(o *option.Option) {
 				command.New(o, virtualMachineRootCmd, "stop").WithoutSuccessfulExit().Run()
 			})
 
-			ginkgo.It("should get nonexistent status", func() {
+			// Test Stopped -> NonExistent
+			ginkgo.It("should be able to remove a stopped vm", func() {
+				command.New(o, virtualMachineRootCmd, "init").WithTimeoutInSeconds(240).Run()
+				command.New(o, virtualMachineRootCmd, "stop").WithTimeoutInSeconds(90).Run()
+				gomega.Expect(command.StdoutStr(o, virtualMachineRootCmd, "status")).To(gomega.Equal("Stopped"))
+				command.New(o, virtualMachineRootCmd, "remove").WithTimeoutInSeconds(90).Run()
 				gomega.Expect(command.StdoutStr(o, virtualMachineRootCmd, "status")).To(gomega.Equal("Nonexistent"))
 			})
+
+			// Following state changes are part of synchronization workflows
+			// Test NonExistent -> Running
 		})
 	})
 }
